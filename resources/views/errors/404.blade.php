1a8927fb78195f2fb04786fabc52b1b97e077ab9
Brian Twitchell <32440651+liataun@users.noreply.github.com>
2019-04-05 20:12:14 -0400

    Added 404 view customization

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('Not Found'))
+@section('code', '404')
+@section('message', __('We were unable to retrieve the resource you requested. Please return to the home page.'))
